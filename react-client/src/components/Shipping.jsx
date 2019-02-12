c84e9c279fa5bd668d483016a7028fddf805c3e5
John Sangiolo <johnsangiolo@gmail.com>
2019-02-11 19:15:25 -0500

    refactored TimeLeft component to display a countdown clock of the remaining time

--- react-client/src/components/Shipping.jsx
+++ react-client/src/components/Shipping.jsx
@@ -3,21 +3,17 @@ import TimeLeft from './styled-components/TimeLeft.jsx';
 import UnitsSold from './styled-components/UnitsSold.jsx';
 import ShippingType from './styled-components/ShippingType.jsx';
 
-const Shipping = ({shipping}) => {
+const Shipping = ({shipping, time}) => {
 
-  const getTimeDiff = (diff) => {
-    let days = Math.floor(diff / 86400)
-    let hours = Math.floor(diff / 3600) % 24
-
-    if (diff < 0) return <TimeLeft style={{color:"rgb(255, 114, 118)"}}>Drop has Closed</TimeLeft>
-    if (days > 1) return <TimeLeft style={{color:"rgb(132, 148, 147)"}}>{`${days} days left`}</TimeLeft>
-    if (days === 1) return <TimeLeft style={{color:"rgb(132, 148, 147)"}}>{`${days} day left`}</TimeLeft>
-    else return <TimeLeft style={{color:"rgb(255, 114, 118)"}}>{`${hours} hours left`}</TimeLeft>
+  const checkTime = (time) => {
+    if (time < 0) return <TimeLeft style={{color:"rgb(255, 114, 118)"}}>Drop has Closed</TimeLeft>
+    if (time.days > 0) return <TimeLeft style={{color:"rgb(132, 148, 147)"}}>{time.days + ':' + time.hours + ':' + time.minutes + ':' + time.seconds}</TimeLeft>
+    else return <TimeLeft style={{color:"rgb(255, 114, 118)"}}>{time.days + ':' + time.hours + ':' + time.minutes + ':' + time.seconds}</TimeLeft>
   }
 
   return(
     <div>
-      {getTimeDiff((new Date(shipping.time * 1000) - new Date()) / 1000)}  
+      {time ? checkTime(time) : ''} 
       <UnitsSold>{`${shipping.units_sold} sold `}</UnitsSold> 
       <ShippingType>{shipping.shipping_option}</ShippingType>
     </div>
